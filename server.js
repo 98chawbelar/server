cb9db0a44be30e735d7faeb09c7474ccf56071be
98chawbelar <130055118+98chawbelar@users.noreply.github.com>
2026-06-04 20:10:20 +0700

    update url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://mrbs-client.vercel.app"],
     credentials: true,
   }),
 );
